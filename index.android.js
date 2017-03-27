d9c3f0d13499ca487c87acb14edcb8229a85edf7
kong <nopaket_kong@hotmail.com>
2017-03-27 19:25:18 +0700

    aa

--- index.android.js
+++ index.android.js
@@ -1,7 +1,10 @@
-var React = require('react-native')
-var { AppRegistry } = React
-var AwesomeAgeApp = require('./AwesomeAgeApp')
-AppRegistry.registerComponent('AwesomeAgeApp', () => AwesomeAgeApp)
+//var React = require('react-native')
+//var { AppRegistry } = React
+
+import ReactCalculator from './src/CalculatorApp';
+
+//var AwesomeAgeApp = require('./AwesomeAgeApp')
+//AppRegistry.registerComponent('AwesomeAgeApp', () => AwesomeAgeApp)
 
 //var HelloWorldApp = require('./HelloWorldApp')
 //AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp)
