89cd6b509e92221989a619b689176eec12ebca50
kong <nopaket_kong@hotmail.com>
2017-03-27 16:35:19 +0700

    test

--- AwesomeAgeApp.js
+++ AwesomeAgeApp.js
@@ -1,73 +1,66 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, TextInput, View} from 'react-native';
 
+export default class AwesomeAgeApp extends Component {
 
-
-export default class AwesomeAgeApp extends Component{
-
-constructor(props) {
-    super(props);
-    this.state = {age: ''};
-  }
-render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.heading} >
-        Age App!!
-        </Text>
-        <Text style={styles.ageLabel}>
-            Enter Your Age : 
-        </Text>
-        <TextInput 
-            style={styles.ageInput}
-            returnKeyType='go'
-            onSubmitEditing={this._handleTextChange}/>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: ''
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.heading}>
+                    Age App!!
+                </Text>
+                <Text style={styles.ageLabel}>
+                    Enter Your Age :
+                </Text>
+                <TextInput
+                    style={styles.ageInput}
+                    returnKeyType='go'
+                    onSubmitEditing={this._handleTextChange}/>
+            </View>
+        );
+    }
 }
 
-
 const styles = StyleSheet.create({
-    container : {
-        backgroundColor : '#F5FCFF',
-        paddingTop : 40,
-        padding : 10,
-        alignItems : 'center',
-        flex :1,
+    container: {
+        backgroundColor: '#F5FCFF',
+        paddingTop: 40,
+        padding: 10,
+        alignItems: 'center',
+        flex: 1
     },
-    heading : {
-        fontSize : 24,
-        textAlign : 'center',
-        margin : 10,
-        color : '#0000CC',
+    heading: {
+        fontSize: 24,
+        textAlign: 'center',
+        margin: 10,
+        color: '#0000CC'
     },
-    ageInput : {
-        height : 50,
-        marginTop : 10,
-        padding : 4,
-        fontSize : 18,
-        borderWidth : 1,
-        borderColor : '#0000CC',
-        borderRadius :0,
-        color : '#0000CC',
+    ageInput: {
+        height: 50,
+        marginTop: 10,
+        padding: 4,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#0000CC',
+        borderRadius: 0,
+        color: '#0000CC'
     },
-    ageLabel : {
-        fontSize : 16,
-        margin:10,
-        marginBottom:20,
+    ageLabel: {
+        fontSize: 16,
+        margin: 10,
+        marginBottom: 20
     },
-    ageResult : {
-        fontSize : 20,
-        padding : 4,
-        margin:10,
-        marginTop : 20,
+    ageResult: {
+        fontSize: 20,
+        padding: 4,
+        margin: 10,
+        marginTop: 20
     }
 })
 
